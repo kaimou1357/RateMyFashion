eebbf590326d035b2484b0994bb124a0b55200d4
Kai Mou <kaimou1357@gmail.com>
2016-02-26 01:34:54 -0500

    Created folders

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ app.use('/images', express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.redirect("/index.html");
+  res.render("/index.html");
 })
 
 app.get('/load_next_picture', function(req, res){
-  var picture_url = "static/images/cat.jpg";
-  var base_url = "localhost:3000/"
+  var picture_url = "/images/cat.jpg";
+  var base_url = "localhost:3000"
+  console.log("Completed!");
   res.json({"picture_url": base_url + picture_url});
 
 });
