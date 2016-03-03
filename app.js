bc6dc9b8c4d11687f11e27a2460d4616555155ed
Kai Mou <kaimou1357@gmail.com>
2016-03-02 23:41:49 -0500

    Photo system working needs database

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ var Photo = require('./photo.js');
 app = express();
 app.use('/images', express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/public'));
-
+var base_url = "localhost:3000/images/";
+var photoArray = [];
+var numOfPhotos = 0;
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './images/');
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + ".jpg");
+      var newPhoto = new Photo(base_url + numOfPhotos + ".jpg", 0 ,0, Math.floor(Math.random()*1000));
+      photoArray.push(newPhoto);
+      cb(null, numOfPhotos++ + ".jpg");
   }
 })
 var upload = multer({storage:storage});
@@ -19,8 +23,7 @@ var upload = multer({storage:storage});
 
 
 //require('./routes/routes.js')(app);
-var base_url = "localhost:3000/images/";
-var photoArray = [];
+
 
 /*
  * To Do:
@@ -31,59 +34,56 @@ var photoArray = [];
  *
 */
 
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/load_next_picture', function(req, res){
-  var picture_url = "/images/cat.jpg";
-  var base_url = "localhost:3000";
-  console.log("Completed!");
+  var photo = photoArray[Math.floor((Math.random() * numOfPhotos))];
+  console.log(numOfPhotos);
   res.json({
-    "file_url": base_url + picture_url,
-    "positive_ratings" : "30%",
-    "negative_ratings": "50%"
+    "file_url": photo.file_url,
+    "positive_ratings" : photo.positive_ratings,
+    "negative_ratings": photo.negative_ratings
   });
 
 });
 
 app.get('/load_ten_pictures', function(req, res){
-  //./ looks into current working directory.
+  // looks into current working directory.
   var tmp = [];
   for(var i = 0; i < 10; i++){
     tmp[i] = photoArray[i];
   }
-  res.json(tmp);
+  res.send(tmp);
 });
 
-app.get('/initialize_all_pictures', function(req, res){
-  var path = './images/';
-  fs.readdir(path, function(err, items){
-    if (err){
-      console.log("there was an error");
-      return;
-    }
-    console.log(items.length);
-    for(var i = 0; i<items.length; i++){
-      var photo = new Photo(base_url + items[i], 0, 0,Math.floor(Math.random()*1000));
-      photoArray.push(photo);
-    }
-
-  });
-  res.json({"Success" : "Done"});
+app.post('/update_picture_rating', function(req, res){
+  //Post request takes two parameters.
+  //-Picture ID
+  //-1(positive) to add one to positive rating, -1 to add to negative rating.
+  if(req == 'POST'){
+      var photoID = req.body.picture_id;
+      var ratingChange = req.body.rating_change;
+      if(ratingChange == 1){
+        photoArray[photoID].positive_ratings++;
+      }
+      else{
+        photoArray[photoID].negative_ratings++;
+      }
+      res.json(photoArray[photoID]);
+  }
+  else{
+    res.json({
+      "file_url": photoArray[photoID].file_url,
+      "positive_ratings" : photoArray[photoID].positive_ratings,
+      "negative_ratings": photoArray[photoID].negative_ratings
+    });
+  }
 });
 
 app.post('/upload',upload.single('image'), function(req, res){
   res.json({"Success": "True"});
 });
-
-app.get('/images/:file', function(req, res){
-  file = req.params.file;
-  var img = fs.readFileSync(__dirname + '/images/' + file);
-  res.writeHead(200, {'Content-Type' : 'image/jpg'});
-  res.end(img, 'binary');
-});
-
-
-
 app.listen(3000);
