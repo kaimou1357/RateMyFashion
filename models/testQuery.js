f5d84d16aa2789b8175abfee3ad85773049f74ba
Kevin Zhang <zhang4444@gmail.com>
2016-04-06 01:39:13 -0400

    Fixed check_user endpoint

--- models/testQuery.js
+++ models/testQuery.js
@@ -4,8 +4,8 @@ var connectionString = "postgres://localhost:5432/fashiondb";
 var client = new pg.Client(connectionString);
 client.connect();
 
-var userID = 'asxapxhsahqjqnolhuolroeulcr';
-var query = client.query("SELECT (user_id, first_name, last_name) FROM users WHERE user_id = $1;", [userID]);
+var userID = 'fa';
+var query = client.query("SELECT (user_id) FROM users WHERE user_id = $1;", [userID]);
 
 var user;
 query.on('row', function(row) {
@@ -13,8 +13,24 @@ query.on('row', function(row) {
 });
 
 query.on('end', function() {
-	if (user === undefined)
-		client.query("INSERT INTO users (user_id) VALUES ($1);", [userID]);
-	else
+	if (user === undefined) {
+		query = client.query("INSERT INTO users (user_id) VALUES ($1) RETURNING (user_id);", [userID]);
+		returnUserJSON(client, query);
+	}
+	else {
 		console.log(user);
+		client.end();
+	}
 });
+
+var returnUserJSON = function(client, query) {
+	var result;
+
+	query.on('row', function(row) {
+		result = row;
+	});
+
+	query.on('end', function() {
+		client.end();
+	});
+}
--- routes/apiEP.js
+++ routes/apiEP.js
@@ -26,7 +26,7 @@ exports.load_photos = function(req, res, next) {
 								 	"LIMIT $2;",
 								 [req.query.user_id, req.query.num]);
 
-		returnJSONArray(query, done, res);
+		returnPhotoJSONArray(query, done, res);
 	});
 }
 
@@ -76,7 +76,7 @@ exports.delete_photo = function(req, res, next) {
 
 		var query = client.query("DELETE FROM photos WHERE photo_id = $1 RETURNING photo_id, likes, dislikes, user_id", [req.params.photo_id]);
 
-		returnJSON(query, done, res);
+		returnPhotoJSON(query, done, res);
 	});
 
 	console.log('deleted photo ' + req.params.photo_id);
@@ -96,7 +96,7 @@ exports.like_photo = function(req, res, next) {
 		client.query("INSERT INTO seen_photos (user_id, photo_id) VALUES ($1, $2);", [req.body.user_id, req.params.photo_id]);
 		var query = client.query("UPDATE photos SET likes = likes + 1 WHERE photo_id = ($1) RETURNING photo_id, likes, dislikes, user_id", [req.params.photo_id]);
 
-		returnJSON(query, done, res);
+		returnPhotoJSON(query, done, res);
 	});
 
 	console.log('liked photo ' + req.params.photo_id);
@@ -116,7 +116,7 @@ exports.dislike_photo = function(req, res, next) {
 		client.query("INSERT INTO seen_photos (user_id, photo_id) VALUES ($1, $2);", [req.body.user_id, req.params.photo_id]);
 		var query = client.query("UPDATE photos SET dislikes = dislikes + 1 WHERE photo_id = ($1) RETURNING photo_id, likes, dislikes, user_id;", [req.params.photo_id]);
 
-		returnJSON(query, done, res);
+		returnPhotoJSON(query, done, res);
 	});
 
 	console.log('disliked photo ' + req.params.photo_id);
@@ -136,13 +136,19 @@ exports.load_own = function(req, res, next) {
 
 		var query = client.query("SELECT (photo_id, likes, dislikes) FROM photos WHERE user_id = $1;", [req.query.user_id]);
 
-		returnJSONArray(query, done, res);
+		returnPhotoJSONArray(query, done, res);
 	});
 
 	console.log('loaded photos for ' + req.query.user_id);
 }
 
-exports.check_user = function(res, res next) {
+/**
+This function checks if the user is in the database and returns the id if they are. If not, it will add
+the user_id into the database and return it. For now Facebook is our only login so Facebook ID will be 
+user_id, but eventually when we integrate other ways of logging in this may not be the case, so while it
+doesn't make sense to return the user_id right now, it will later on.
+*/
+exports.check_user = function(req, res, next) {
 	pg.connect(connectionString, function(err, client, done){
 		if (err) {
 			done();
@@ -150,7 +156,7 @@ exports.check_user = function(res, res next) {
 			return res.status(500).json({success: false, data: err});
 		}
 
-		var query = client.query("SELECT (user_id, first_name, last_name) FROM users WHERE user_id = $1;", [req.query.user_id]);
+		var query = client.query("SELECT (user_id) FROM users WHERE user_id = $1;", [req.query.user_id]);
 
 		var user;
 		query.on('row', function(row) {
@@ -160,22 +166,20 @@ exports.check_user = function(res, res next) {
 		query.on('end', function() {
 			if (user === null) {
 				query = client.query("INSERT INTO users (user_id) VALUES ($1) RETURNING (user_id);", [req.query.user_id]);
-				returnJSON(query, done, res);
+				returnUserJSON(query, done, res);
 			}
 			else {
 				done();
 				return res.json(user);
 			}
 		});
-
-		returnJSONArray(query, done, res);
 	});
 
 	console.log('checked user ' + req.query.user_id)
 }
 
 //helper functions
-var returnJSONArray = function(query, done, res) {
+var returnPhotoJSONArray = function(query, done, res) {
 	var result = [];
 
 	query.on('row', function(row) {
@@ -189,11 +193,24 @@ var returnJSONArray = function(query, done, res) {
 	});
 }
 
-var returnJSON = function(query, done, res) {
+var returnPhotoJSON = function(query, done, res) {
 	var result;
 
 	query.on('row', function(row){
 		row.file_url =  baseFileURL + row.photo_id + '.jpg';
+		result = row;
+	});
+
+	query.on('end', function() {
+		done();
+		return res.json(result);
+	});
+}
+	
+var returnUserJSON = function(query, done, res) {
+	var result;
+
+	query.on('row', function(row) {
 		result = row;
 	});
 
