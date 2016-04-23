3fd937b4b7e73871f7abf136f3591d1e8c1b797a
Kevin Zhang <zhang4444@gmail.com>
2016-04-23 13:35:47 -0400

    Made likes and dislikes default to 0

--- models/database.js
+++ models/database.js
@@ -7,7 +7,7 @@ var client = new pg.Client(connectionString);
 client.connect();
 //Create user and photo tables.
 var query = client.query("CREATE TABLE users(user_id text UNIQUE, first_name text, last_name text);" +
-						 "CREATE TABLE photos(photo_id SERIAL PRIMARY KEY, likes integer, dislikes integer, user_id text);" +
+						 "CREATE TABLE photos(photo_id SERIAL PRIMARY KEY, likes integer DEFAULT 0, dislikes integer DEFAULT 0, user_id text);" +
 						 "CREATE TABLE seen_photos(user_id text, photo_id integer);");
 query.on('end', function(){
 	client.end();
